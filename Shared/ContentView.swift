18727f811ae6b27b0b3714cdf509413860d5cbc3
Simon Anderson <simonbenanderson@gmail.com>
2021-01-07 17:19:18 +1300

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Simon Anderson on 7/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/CustomMetalViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomMetalViewApp.swift
+//  Shared
+//
+//  Created by Simon Anderson on 7/01/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomMetalViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
